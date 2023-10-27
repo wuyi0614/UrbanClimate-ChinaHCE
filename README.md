b893db26441f6336d689f0ea14c25d642136ef06
Yi Wu <wymario@163.com>
2023-10-27 17:39:17 +0100

    data merging and transformation

--- README.md
+++ README.md
@@ -3,13 +3,15 @@ The repo for the data and codes from the paper about China's HCE to be published
 
 ## Outline
 
-*Edited on 18 Oct 2023*
+*Edited on 27 Oct 2023*
 
-*Title: City-level household carbon emission typology and implications: a machine learning-based approach(pending)*
+Notably, as accounting the carbon emission based on the survey is rather difficult, we use energy consumption instead for analysis.
+
+*Title: City-level household energy consumption typology and implications: a machine learning-based approach(pending)*
 - [ ] Abstract (dependent on findings) - YW
 - [ ] Introduction & Literature Review
-  - [ ] Cities account for a majority of emissions & household energy/lifestyle emissions
-  - [ ] Understanding of Chinese cities' emission and HCEs
+  - [ ] Cities account for a majority of energy consumption & household energy/lifestyle energy consumption
+  - [ ] Understanding of Chinese cities' energy consumption and emissions
   - [ ] What approaches have been applied to explore HCEs and what findings regarding urban/rural HCEs
   - [ ] What gap exists in the current researches? -- highly dependent on a fixed range of factors, and lack of deep understanding of lifestyle and behaviors of Households
 - [ ] Methodology and data description (& a graphic methodology)
@@ -19,7 +21,7 @@ The repo for the data and codes from the paper about China's HCE to be published
 - [ ] Results
   - [ ] City-level energy consumption and emissions - YW
     - [ ] city-level policies 
-    - [ ] city-level percap emissions / percap HCEs
+    - [ ] city-level percap energy consumption
     - [ ] a chart showing the average city percap emission and urban/rural hce
   - [ ] HCE inequality by regions - YW & ZYX
   - [ ] A machine learning-based HCE typology - ZYX
--- result_one.py
+++ result_one.py
@@ -3,30 +3,75 @@
 # Created on 18 Oct 2023, by Yi
 #
 
+import json
 import pandas as pd
 
 from pathlib import Path
 
+
 # env variables
-DATAFILE = Path('data') / 'rawdata-0125.csv'
-CALCULATE_DATAFILE = Path('data') / 'CGSS-calculate-20231019.xlsx'
+MAPPINGFILE = Path('data') / 'mapping.json'
+MAPPING = json.loads(MAPPINGFILE.read_text(encoding='utf8'))
 
 
 # functions
-def get_cities(data: pd.DataFrame) -> list:
+def get_cities(data: pd.DataFrame) -> pd.DataFrame:
     """
-    Return a list of city names
+    Transform cities in Chinese into English
 
     :param data: the raw dataframe
-    :return: list
+    :return: an updated dataframe
     """
+    city = MAPPING['city']
+    data['prefecture_en'] = data['prefecture'].apply(lambda x: city.get(x, 'other'))
+    return data
+
+
+def get_percap(data: pd.DataFrame, columns: list) -> pd.DataFrame:
+    """
+    Transform selected data into percap data
+
+    :param data: the raw dataframe
+    :param columns: columns for percap transformation
+    :return: an updated dataframe
+    """
+    for c in columns:
+        data[f'{c}_percap'] = data[c] / data['size']
+
+    return data
+
+
+def get_energy(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transform energy consumption data into a well-shaped dataset
+
+    :param data: the raw energy consumption dataframe
+    :return: the pivot-processed dataframe
+    """
+    data['type'] = data['type'].apply(lambda x: f'en_{x}')
+    merged = data[['id', 'type', 'use']].groupby(['id', 'type']).sum().reset_index()
+    pivot = merged.pivot(index='id', columns='type', values='use').fillna(0)
+    pivot['en_total'] = pivot.iloc[:, 1:].sum(axis=1)  # sum by rows
+    return pivot
+
+
+def analyse(data: pd.DataFrame) -> pd.DataFrame:
+
 
     return
 
 
-
-
 if __name__ == '__main__':
-    # test
-    data = pd.read_csv(DATAFILE)
-    pass
+    # merging data and test
+    datafile = Path('data') / 'vardata-1025.xlsx'
+    data = pd.read_excel(datafile, engine='openpyxl')
+    engfile = Path('data') / 'energyuse-1024.xlsx'
+    eng = pd.read_excel(engfile, engine='openpyxl')
+    eng = get_energy(eng)
+
+    merged = data.merge(eng, on='id', how='left')
+    # preprocess
+    merged = get_cities(merged)
+    merged = get_percap(merged, columns=['en_total'])
+
+
