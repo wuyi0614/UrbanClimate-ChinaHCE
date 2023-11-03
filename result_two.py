11d205fb079d76bd66c85e50bf1ca1d49ecd393c
Yi Wu <wymario@163.com>
2023-11-03 17:17:36 +0000

    staged datafiles and imgs

---
+++ result_two.py
@@ -0,0 +1,15 @@
+# Replicable script for the second section of Results
+#
+# Created on 31 Oct 2023, by Yi
+#
+
+# In this section, we will basically use GINI index and xxx to measure the inequality between
+# regions, cities, and groups
+
+def inequality(data):
+
+    return
+
+
+if __name__ == '__main__':
+    pass
